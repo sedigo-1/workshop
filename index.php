1b53d39e44142c70e2c53ea91a4a7e4a4df237c4
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-02-06 08:46:19 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 <html>
 <head>
 
-<title>WEB DESIGN 1</title>
+<title> HERNEL WEB DESIGN </title>
 </head>
+
 <body style="background-color:gray;">
 
 <center><h1 style="background-color:LightGray;">PERSONAL DATA</h1></center>
+<img src="kokey.jpg" size="100">
 <p><h2>NAME:<u>Hernel A. Sedigo</u></h2></p>
 <p><h2>AGE N SEX:<u>21 Male</u></h2></p>
 <p><h2>ADDRESS:<u>St.Joseph Village Tunghaan Minglanilla Cebu</u></h2></p>
@@ -20,14 +22,14 @@
 <p><h2>HIGH SCHOOL:<u>Inayagan National High School</u></h2></p>
 <p><h2>COLLEGE:<u>Cebu Technological University</u></h2></p><hr>
 <center><h1 style="background-color:LightGray">BIOGRAPHY</h1></center>
-<p><h2>When I was a 9yrs old, I really liked playing playstaion games with my friend . 
-When I was in high school where computers are known in the year 2010 .
-I always absent in the class just to play computer.
-I was addicted to the computer games, and there was a problem with my grades and I lost it.
-I had my summer classes just to graduate in high school.
-After, I graduate i entered any kind of jobs.
-after all i realize to my self that i will continue my college.
-Thats why i choose to this course becouse i want to make my own online game. 
+<p><h2>When i was 10 yrs old ,i used to play playstation together with my friends.
+When i was in high school, it is the time where the computer known,
+i spend a lot of my time playing online games on it, i even make a lot of absences just to play .
+I noticed that i am addicted into it , as a result i got a low grades and the worst is i failed
+my other subjects . I had to undergo summer classes just to graduate in high school.
+after i graduate , I've been into defferent kinds of job, i experience how difficult life would 
+be if you were just a high school graduated , and so , i decided to continue my studies and procced
+to college. I choose this course because i am a computer enthusiast and i want to create my own online game.
 </h2></p>
 </body>
 </html>
